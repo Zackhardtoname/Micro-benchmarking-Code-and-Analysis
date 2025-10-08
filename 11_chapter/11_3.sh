7e7689b80e94ae64b7859e3b22a6cdf7b4fd6945
Zackhardtoname <zack.hardtoname@gmail.com>
2025-10-08 16:34:51 -0400

    Add 11_3 script

--- 11_chapter/11_3.sh
+++ 11_chapter/11_3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Run and time Whetstone benchmark with incremental flags using perf
-#
-cd ../book_files
+# Run with ./11_3.sh > 11_3.txt 2>&1
+
 SRC_C="whetstone_ku.c"
-SRC_CC="kutrace_lib.cc"
+SRC_CC="../book_files/kutrace_lib.cc"
 OUT_BASE="whetstone_ku"
 FLAGS_BASE="-O2"
 EXTRA_FLAGS=(
@@ -14,6 +14,7 @@ EXTRA_FLAGS=(
     "-pg"
     "-g"
 )
+ARG=1000000
 
 echo "===== Running Whetstone with incremental compilation flags (perf) ====="
 
@@ -25,15 +26,16 @@ for i in $(seq 0 ${#EXTRA_FLAGS[@]}); do
 
     EXE="${OUT_BASE}_$i"
     echo -e "\n--- Building $EXE with flags: $FLAGS ---"
-    g++ $FLAGS $SRC_C $SRC_CC -lm -o $EXE
+    g++ -I ../book_files $FLAGS $SRC_C $SRC_CC -lm -o $EXE
+    CMD="$EXE $ARG"
 
     echo "--- Running $EXE (normal run) ---"
-    /usr/bin/time ./$EXE
+    /usr/bin/time ./$CMD
 
     echo "--- Profiling $EXE with perf ---"
     PERF_DATA="perf_data_$i.data"
     rm -f "$PERF_DATA"
-    perf record -o "$PERF_DATA" -q ./$EXE
+    perf record -o "$PERF_DATA" -q ./$CMD
 
     echo "--- Generating short perf report for $EXE ---"
     perf report -i "$PERF_DATA" --stdio | head -n 10
@@ -42,11 +44,12 @@ done
 # For the last executable, produce detailed annotated report
 FINAL_ID=${#EXTRA_FLAGS[@]}
 FINAL_EXE="${OUT_BASE}_${FINAL_ID}"
+FINAL_CMD="${OUT_BASE}_${FINAL_ID} ${ARG}"
 FINAL_DATA="perf_data_final.data"
 
 echo -e "\n===== Generating detailed perf report for $FINAL_EXE ====="
 rm -f "$FINAL_DATA"
-perf record -o "$FINAL_DATA" -q ./$FINAL_EXE
+perf record -o "$FINAL_DATA" -q ./$FINAL_CMD
 
 echo "--- Summary report ---"
 perf report -i "$FINAL_DATA" --stdio > perf_summary.txt
